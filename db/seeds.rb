53630154f646c9ff2df192789d94dd1a69103314
YOUR NAME <muvanlier@gmail.com>
2017-07-11 22:08:21 +0200

    added list of artist

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+artist1 = Artist.create!( {name: "Bob Dylan"} )
+artist2 = Artist.create!( {name: "Craig David"} )
+artist3 = Artist.create!( {name: "Tupac"} )
+artist4 = Artist.create!( {name: "Bob Marley"} )
+artist5 = Artist.create!( {name: "Allen Rayman"} )
+artist6 = Artist.create!( {name: "Charles Bradley"} )
+artist7 = Artist.create!( {name: "David Bowie"} )
+artist8 = Artist.create!( {name: "Jim Morrissey"} )
